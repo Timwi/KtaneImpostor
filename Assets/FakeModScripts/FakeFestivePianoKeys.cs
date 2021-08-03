0d0db4165cead6797d354b3a36096fed87dedcaf
Blananas2 <blananas2@gmail.com>
2021-08-03 18:50:06 -0400

    Add Semaphore, and all 3 main Piano Keys variants
    I shoulda probably added quirks where CPK has 3 symbols and the other two having 4 but I'm lazy

---
+++ Assets/FakeModScripts/FakeFestivePianoKeys.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KModkit;
+using System.Linq;
+using Rnd = UnityEngine.Random;
+
+public class FakeFestivePianoKeys : ImpostorMod 
+{
+    [SerializeField]
+    private TextMesh display; //SerializeField causes the variable to show up in the inspector, while keeping it a private variable.
+
+    private int Case;
+
+    void Start()
+    {
+        flickerObjs.Add(display.gameObject);
+        string set = "mB\"%x*v^w>";
+        List<string> symbols = new List<string> {};
+        PKretry:
+        for (int i = 0; i < 3; i++) {
+            symbols.Add(set.PickRandom().ToString());
+        }
+        if ((symbols[0] == symbols[1]) || (symbols[0] == symbols[2]) || (symbols[1] == symbols[2])) {
+            Log("the display has identical symbols");
+        } else {
+            symbols.Clear();
+            goto PKretry;
+        }
+        display.text = symbols[0] + "  " + symbols[1] + "  " + symbols[2];
+    }
+}
